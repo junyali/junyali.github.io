d10cba4a31d14eb20d276706dfdf6593210cbb1e
Junya <junjia.liu@pm.me>
2025-02-17 19:34:56 +0000

    Add navigation bar

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,22 @@
 particlesJS.load('particles-js', './particles.json', function() {
   console.log('particles.js loaded - callback');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const navbar = document.querySelector('.navbar');
+  let lastScrollTop = 0;
+
+  window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+      // Scrolling down
+      navbar.classList.add('shrink');
+    } else {
+      // Scrolling up
+      navbar.classList.remove('shrink');
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  });
+});
