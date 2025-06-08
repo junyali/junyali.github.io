267b8b281e5537e9d831faabd30d34da55056a5d
Junya <junjia.liu@pm.me>
2025-06-08 21:02:43 +0100

    Fix GH WF Deploy to Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: "/junyali.github.io/",
+    base: "/",
     build: {
         outDir: "dist",
         assetsDir: "assets",
