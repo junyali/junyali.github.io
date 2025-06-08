3c1bfdf4c6f042e809f492cdcfb5e086e0c31183
Junya <junjia.liu@pm.me>
2025-06-08 20:53:52 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    base: "/junyali.github.io/",
+    build: {
+        outDir: "dist",
+        assetsDir: "assets"
+    },
     plugins: [
         tailwindcss(),
     ],
